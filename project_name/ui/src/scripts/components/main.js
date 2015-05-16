18a4cedcaaaba090b9e827fe35eee01b1eff922a
Matt Hollingsworth <matt@deepfield.net>
2015-05-16 18:18:55 -0400

    Add example import of util.js

--- project_name/ui/src/scripts/components/main.js
+++ project_name/ui/src/scripts/components/main.js
@@ -2,6 +2,7 @@ var App = require('./App');
 var React = require('react');
 var Router = require('react-router');
 var Route = Router.Route;
+var util = require('./util.js');
 
 var Routes = (
   <Route handler={App}>
